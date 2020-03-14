9e4c30a3b98fcead729ab40c18c7ec31f320133d
yiqiaoxihui <937285794@qq.com>
2020-03-14 23:40:45 +0800

    print want you want in program for debug or others

---
+++ mfc_print_debug.c
@@ -0,0 +1,22 @@
+
+
+void inline print_error(std::string str)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED);
+	_cprintf("Error:%s\n",str.c_str());
+}
+void inline print_warning(std::string str)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN);
+	_cprintf("Warning:%s\n",str.c_str());
+}
+void inline print_normal(std::string str)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7);
+	_cprintf("INFO:%s\n",str.c_str());
+}
+void inline print_green(std::string str)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN);
+	_cprintf("INFO:%s\n",str.c_str());
+}
